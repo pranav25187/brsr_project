669251cc6fdb9a87d0894af726c4be5a1ed74d45
pranav25187 <pranavborse2023@gmail.com>
2026-04-27 23:02:47 +0530

    Update api.ts

--- Post_services2/frontend/src/api.ts
+++ Post_services2/frontend/src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { User } from "./slices/authSlice"; // adjust if needed
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
 // ---------------- Response Handler ----------------
